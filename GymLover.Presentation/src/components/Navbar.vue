f8e859f56eb16beca779c7b3757cdd1c1ef7fccd
RenatoChiodi <renatod.chiodi@gmail.com>
2021-10-20 07:24:23 -0300

    Melhorias no menu

---
+++ GymLover.Presentation/src/components/Navbar.vue
@@ -0,0 +1,163 @@
+<template>
+
+<div id="app">
+    <v-app>
+        <v-app-bar app color="primary" dark elevation="0">
+            <v-app-bar-nav-icon @click.stop="sidebarMenu = !sidebarMenu"></v-app-bar-nav-icon>
+            <v-spacer></v-spacer>
+            <v-btn @click="toggleTheme" color="primary" class="mr-2">{{buttonText}}</v-btn>
+            <v-icon>mdi-account</v-icon>
+        </v-app-bar>
+        <v-navigation-drawer v-model="sidebarMenu" app floating :permanent="sidebarMenu" :mini-variant.sync="mini">
+            <v-list dense color="primary" dark>
+                <v-list-item>
+                    <v-list-item-action>
+                        <v-icon @click.stop="sidebarMenu = !sidebarMenu">mdi-chevron-left</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>
+                            <h3 class="font-weight-thin">Vuetify Dashboard</h3>
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+            <v-list-item class="px-2" @click="toggleMini = !toggleMini">
+                <v-list-item-avatar>
+                    <v-icon>mdi-account-outline</v-icon>
+                </v-list-item-avatar>
+                <v-list-item-content class="text-truncate"> Jane Doeski </v-list-item-content>
+                <v-btn icon small>
+                    <v-icon>mdi-chevron-left</v-icon>
+                </v-btn>
+            </v-list-item>
+            <v-divider></v-divider>
+            <v-list>
+                <v-list-item v-for="item in items" :key="item.title" link :to="item.href">
+                    <v-list-item-icon>
+                        <v-icon color="primary">{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title class="primary--text">{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+        <v-content>
+            <v-container class="px-4 py-0 fill-height" fluid>
+                <v-row class="fill-height">
+                    <v-col>
+                        <transition name="fade">
+                            <router-view></router-view>
+                        </transition>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-content>
+        <v-footer class="py-3">
+            <span class="ml-auto overline">Iatek &copy;2020</span>
+        </v-footer>
+    </v-app>
+</div>
+  
+</template>
+<script>
+import { defineComponent } from "@vue/composition-api";
+import Vue from 'vue'
+
+
+Vue.component('sidebar', {
+    template: '#navbar',
+    props: {
+        items: Array
+    },
+})
+
+
+export default defineComponent({
+  setup() {},
+  props: ["rotas"],
+  computed: {
+    mini() {
+        return (this.$vuetify.breakpoint.smAndDown) || this.toggleMini
+    },
+    buttonText() {
+        return !this.$vuetify.theme.dark ? 'Go Dark' : 'Go Light'
+    }
+  },
+  data: () => ({
+    sidebarMenu: true,
+    items: [
+        { title:"Home", href:"/", icon:"mdi-home-outline" },
+        { title:"Alunos", href:"/alunos", icon:"mdi-account-search-outline" },
+      ],
+    drawer: true,
+    group: null,
+  }),
+  methods: {
+    toggleTheme() {
+        this.$vuetify.theme.dark = !this.$vuetify.theme.dark;      
+    }
+    }
+});
+</script>
+<style scoped>
+.navColor {
+  color: #763aa7;
+}
+.menuItem {
+  text-decoration-color: black;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition-property: opacity;
+  transition-duration: .25s;
+}
+
+.fade-enter-active {
+  transition-delay: .25s;
+}
+
+.fade-enter, .fade-leave-active {
+  opacity: 0
+}
+
+.theme--light.v-application ::-webkit-scrollbar {
+  width: 13px;
+}
+
+.theme--light.v-application ::-webkit-scrollbar-track {
+  background: #e6e6e6;
+  border-left: 1px solid #dadada;
+}
+
+.theme--light.v-application ::-webkit-scrollbar-thumb {
+  background: #b0b0b0;
+  border: solid 3px #e6e6e6;
+  border-radius: 7px;
+}
+
+.theme--light.v-application ::-webkit-scrollbar-thumb:hover {
+  background: black;
+}
+
+.theme--dark.v-application ::-webkit-scrollbar {
+  width: 13px;
+}
+
+.theme--dark.v-application ::-webkit-scrollbar-track {
+  background: #202020;
+  border-left: 1px solid #2c2c2c;
+}
+
+.theme--dark.v-application ::-webkit-scrollbar-thumb {
+  background: #3e3e3e;
+  border: solid 3px #202020;
+  border-radius: 7px;
+}
+
+.theme--dark.v-application ::-webkit-scrollbar-thumb:hover {
+  background: white;
+}
+
+
+</style>
